dac274d05a17ea9e516c560c028e0e7c7c806948
Tomas Ghilino <tomasghilino01@gmail.com>
2022-04-07 15:22:08 -0300

    video optimization

--- src/components/ui/Map/MapElements.jsx
+++ src/components/ui/Map/MapElements.jsx
@@ -1,11 +1,11 @@
-import styled from "styled-components";
+import styled from "styled-components"
 
 export const CustomIFrame = styled.iframe`
-    width: 100%;
-    height: 800px;
-    border: 0;
+  width: 100%;
+  height: 800px;
+  border: 0;
 
-    @media(max-width: 768px) {
-        height: 550px;
-    }
-`;
+  @media (max-width: 768px) {
+    height: 550px;
+  }
+`
